2e33baf6cc062e742178bcd3db01fe93e98dce1f
Sharif Olorin <sio@tesser.org>
2014-03-10 17:57:34 +1100

    Support multipart and string output

--- zmq_subscribe/subscribe.go
+++ zmq_subscribe/subscribe.go
@@ -7,33 +7,37 @@ import (
 	"fmt"
 )
 
+func Exitf(code int, format string, v... interface{}) {
+	fmt.Fprintf(os.Stderr, "Fatal: ")
+	fmt.Fprintf(os.Stderr, format, v...)
+	os.Exit(code)
+}
+
 func main() {
 	block := flag.Bool("block", true, "Use a blocking zmq_recv.")
+	text := flag.Bool("text", false, "Read messages as strings.")
+	multipart := flag.Bool("multipart", false, "Read multipart messages and print the parts individually.")
 	flag.Usage = func() {
-		fmt.Fprintf(os.Stderr, "Usage of %s:\n", os.Args[0])
-		fmt.Fprintf(os.Stderr, "%s <zmq_endpoint> [opts]\n", os.Args[0])
+		Exitf(2, "Usage of %s:\n", os.Args[0])
+		Exitf(2, "%s <zmq_endpoint> [opts]\n", os.Args[0])
 		flag.PrintDefaults()
 	}
 	flag.Parse()
 	if flag.NArg() < 1 {
 		flag.Usage()
-		os.Exit(1)
 	}
 	uri := flag.Arg(0)
 	sock, err := zmq.NewSocket(zmq.SUB)
 	if err != nil {
-		fmt.Fprintf(os.Stderr, "Could not create ZMQ socket: %v", err)
-		os.Exit(2)
+		Exitf(2, "Could not create ZMQ socket: %v", err)
 	}
 	err = sock.Connect(uri)
 	if err != nil {
-		fmt.Fprintf(os.Stderr, "Could not connect to endpoint %s: %v", uri, err)
-		os.Exit(2)
+		Exitf(2, "Could not connect to endpoint %s: %v", uri, err)
 	}
 	err = sock.SetSubscribe("")
 	if err != nil {
-		fmt.Fprintf(os.Stderr, "Could not subscribe: %v", err)
-		os.Exit(2)
+		Exitf(2, "Could not subscribe: %v", err)
 	}
 	var recvFlag zmq.Flag
 	if *block {
@@ -42,11 +46,28 @@ func main() {
 		recvFlag = zmq.DONTWAIT
 	}
 	for {
-		msg, err := sock.RecvBytes(recvFlag)
-		if err != nil {
-			fmt.Fprintf(os.Stderr, "Could not receive bytes: %v", err)
-			os.Exit(2)
+		if *multipart && *text {
+			msg, err := sock.RecvMessage(recvFlag)
+			if err != nil {
+				Exitf(2, "Could not receive bytes: %v", err)
+			}
+			for _, part := range msg {
+				fmt.Println(part)
+			}
+		} else if *multipart {
+			msg, err := sock.RecvMessageBytes(recvFlag)
+			if err != nil {
+				Exitf(2, "Could not receive bytes: %v", err)
+			}
+			for _, part := range msg {
+				os.Stdout.Write(part)
+			}
+		} else if *text {
+			msg, err := sock.Recv(recvFlag)
+			if err != nil {
+				Exitf(2, "Could not receive bytes: %v", err)
+			}
+			fmt.Println(msg)
 		}
-		os.Stdout.Write(msg)
 	}
 }
